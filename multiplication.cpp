ed312a292f52ea837aa96fff44831ad2d3af83db
Poorvika <152640996+poorvikaa08@users.noreply.github.com>
2024-09-07 22:14:45 +0530

    multiplication.cpp

--- multiplication.cpp
+++ multiplication.cpp
@@ -5,12 +5,12 @@ int main(){
 
     int n;
 
-    cout<<"Enter the number: ";
-    cin>>n;
+    cout << "Enter the number: ";
+    cin >> n;
 
-    for(int i=1;i<=10;i++)
+    for(int i=1; i<=10; i++)
     {
-        cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+        cout << n << " * " << i << " = " << n*i << endl;
     }
     
     return 0;
